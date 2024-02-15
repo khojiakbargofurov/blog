44166c3546b68300a65075c42b9d91bcafa97169
Khojiakbar <harry.gafurov@gmail.com>
2024-02-15 13:55:35 +0500

    dark mode in center

--- details.js
+++ details.js
@@ -1,23 +1,28 @@
 const BASE_URL = 'https://dummyapi.io/data/v1';
-const API_KEY = '65cb394056458ec3e87c58b3 ';
+const API_KEY = '65cb394056458ec3e87c58b3';
 
+// Function to format date using Luxon
 function formatDate(dateTimeString) {
     const dt = luxon.DateTime.fromISO(dateTimeString);
     return dt.toFormat('dd-MM-yyyy HH:mm');
 }
 
+// Async function to fetch post details
 async function fetchPostDetails() {
     try {
         const urlParams = new URLSearchParams(window.location.search);
         const postId = urlParams.get('id');
         if (!postId) throw new Error('Post ID not provided.');
 
+        // Fetch post details using Axios
         const response = await axios.get(`${BASE_URL}/post/${postId}`, {
             headers: { 'app-id': API_KEY }
         });
         const post = response.data;
+        
+        // Render post details on the webpage
         const postDetailsContainer = document.getElementById('postDetails');
-        const image = post.image ? '<img src="' + post.image + '" class="w-full h-auto mb-4" alt="Post Image">' : '';
+        const image = post.image ? `<img src="${post.image}" class="w-full h-auto mb-4" alt="Post Image">` : '';
         postDetailsContainer.innerHTML = `
             ${image}
             <h2 class="text-xl font-semibold mb-2">${post.text}</h2>
@@ -31,4 +36,3 @@ async function fetchPostDetails() {
 }
 
 fetchPostDetails();
-  
