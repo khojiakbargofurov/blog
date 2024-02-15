ab28efc7b69e2b35b9bd9f247cdf0230df45d055
Khojiakbar <harry.gafurov@gmail.com>
2024-02-15 15:07:34 +0500

    DETAILS PAGE

--- details.js
+++ details.js
@@ -54,7 +54,7 @@ async function fetchPostDetails() {
         const firstTag = post.tags.length > 0 ? post.tags[0] : '';
         postDetailsContainer.innerHTML = `
                     ${image}
-                    <h2 class="text-xl font-semibold mb-2">${post.text}</h2>
+                    <h2 class="text-xl font-semibold mb-2 tracking-tight text-gray-900 dark:text-white">${post.text}</h2>
                     <p class="text-sm text-gray-500">Published by ${post.owner.firstName} ${post.owner.lastName} on ${formatDate(post.publishDate)}</p>
                     <p class="text-sm text-gray-500">Likes: ${post.likes}</p>
                     <p class="text-sm text-gray-500">Tags: ${firstTag}</p>
